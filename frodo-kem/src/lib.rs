b57e1c09f915431ce552955bba73885b0db5ae9c
Michael Lodder <mike@litprotocol.com>
2024-11-07 09:54:34 -0700

    move lints to Cargo.toml

--- frodo-kem/src/lib.rs
+++ frodo-kem/src/lib.rs
@@ -58,21 +58,6 @@
 //! the necessary traits and models for creating a custom implementation.
 //! Be warned, this is not recommended unless you are sure of what you are doing.
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
-#![warn(
-    clippy::mod_module_files,
-    clippy::unwrap_used,
-    missing_docs,
-    missing_debug_implementations,
-    missing_copy_implementations,
-    rust_2018_idioms,
-    trivial_casts,
-    trivial_numeric_casts,
-    unused_extern_crates,
-    unused_import_braces,
-    unused_lifetimes,
-    unused_parens,
-    unused_qualifications
-)]
 
 #[cfg(not(any(
     feature = "efrodo640aes",
