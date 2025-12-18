f6595e5c7928c47080d3069a184494472da368a0
Tony Arcieri <bascule@gmail.com>
2025-12-18 08:40:43 -0700

    Remove `bincode` dev-dependency
    `bincode` is unmaintained. See https://crates.io/crates/bincode/3.0.0

--- frodo-kem/src/lib.rs
+++ frodo-kem/src/lib.rs
@@ -1741,9 +1741,4 @@ mod tests {
         postcard::to_stdvec,
         postcard::from_bytes
     );
-    serde_test!(
-        serialization_bincode,
-        bincode::serialize,
-        bincode::deserialize
-    );
 }
